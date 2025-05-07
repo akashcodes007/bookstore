bc0a5851838dc9107ec93d0bfe4ed3123b4de97d
akashcodes007 <akofficial015@gmail.com>
2025-05-07 16:50:31 +0530

    final changes

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb://<username>:<password>@ac-xyz.mongodb.net:27017/myDB?ssl=true&replicaSet=atlas-abc-shard-0&authSource=admin&retryWrites=true&w=majority';
